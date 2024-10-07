d39452a74ef7944fa497d24fe15de5e3a0eabdc6
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-06 23:24:59 -0400

    Missing packages

--- scripts/lib.js
+++ scripts/lib.js
@@ -2,11 +2,8 @@
 
 import fs from 'fs-extra';
 import path from 'node:path';
-import url from "node:url";
+import url from 'node:url';
 import child_process from 'node:child_process';
-import { default as _yargs } from 'yargs';
-
-const yargs = _yargs(process.argv);
 
 const __dirname = path.parse(url.parse(import.meta.url).pathname).dir;
 
@@ -20,10 +17,11 @@ export const build = (v) =>
         [
             'asc',
             v,
-            ...('--target release --measure -Ospeed --noAssert --optimizeLevel 3 --shrinkLevel 2 --converge --disable mutable-globals,sign-extension,nontrapping-f2i,bulk-memory --runtime stub --memoryBase 0 --lowMemoryLimit --uncheckedBehavior never --initialMemory 1 --maximumMemory 512 --outFile build/' + path.parse(v).name + ".wasm").split(' ')
-//            ...('--target release --measure -Ospeed --noAssert --optimizeLevel 3 --shrinkLevel 2 --outFile build/' + path.parse(v).name + ' --exportRuntime --converge --disable sign-extension,nontrapping-f2i,bulk-memory --runtime stub --memoryBase 0 --lowMemoryLimit --uncheckedBehavior never --initialMemory 1 --maximumMemory 512' + (yargs.argv.notransform ? '' : ' --transform opnet-callable/transform')).split(
- //               ' ',
-  //          )
+            ...(
+                '--target release --measure -Ospeed --noAssert --optimizeLevel 3 --shrinkLevel 2 --converge --disable mutable-globals,sign-extension,nontrapping-f2i,bulk-memory --runtime stub --memoryBase 0 --lowMemoryLimit --uncheckedBehavior never --initialMemory 1 --maximumMemory 512 --outFile build/' +
+                path.parse(v).name +
+                '.wasm'
+            ).split(' '),
         ],
         { stdio: 'inherit' },
     );
