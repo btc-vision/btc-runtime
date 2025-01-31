d82c73d5132a6cde64edeaf794b0325f01bda765
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-01-30 23:19:04 -0500

    Update TransferHelper.ts

--- runtime/shared-libraries/TransferHelper.ts
+++ runtime/shared-libraries/TransferHelper.ts
@@ -1,10 +1,6 @@
 import { u256 } from '@btc-vision/as-bignum/assembly';
-import { BytesWriter } from '../buffer/BytesWriter';
-import { Blockchain } from '../env';
 import { encodeSelector, Selector } from '../math/abi';
 import { Address } from '../types/Address';
-import { Revert } from '../types/Revert';
-import { ADDRESS_BYTE_LENGTH, SELECTOR_BYTE_LENGTH, U256_BYTE_LENGTH } from '../utils';
 
 export const TransferStr = 'transfer(address,uint256)';
 export const ApproveStr = 'approve(address,uint256)';
@@ -24,7 +20,7 @@ export class TransferHelper {
     }
 
     public static safeApprove(token: Address, spender: Address, amount: u256): void {
-        const calldata = new BytesWriter(
+        /*const calldata = new BytesWriter(
             SELECTOR_BYTE_LENGTH + ADDRESS_BYTE_LENGTH + U256_BYTE_LENGTH,
         );
         calldata.writeSelector(this.APPROVE_SELECTOR);
@@ -36,11 +32,11 @@ export class TransferHelper {
 
         if (!isOk) {
             throw new Revert(`TransferHelper: APPROVE_FAILED`);
-        }
+        }*/
     }
 
     public static safeTransfer(token: Address, to: Address, amount: u256): void {
-        const calldata = new BytesWriter(
+        /*const calldata = new BytesWriter(
             SELECTOR_BYTE_LENGTH + ADDRESS_BYTE_LENGTH + U256_BYTE_LENGTH,
         );
         calldata.writeSelector(this.TRANSFER_SELECTOR);
@@ -52,13 +48,14 @@ export class TransferHelper {
 
         if (!isOk) {
             throw new Revert(`TransferHelper: TRANSFER_FAILED`);
-        }
+        }*/
     }
 
     public static safeTransferFrom(token: Address, from: Address, to: Address, amount: u256): void {
-        const calldata = new BytesWriter(
+        /*const calldata = new BytesWriter(
             SELECTOR_BYTE_LENGTH + ADDRESS_BYTE_LENGTH * 2 + U256_BYTE_LENGTH,
         );
+
         calldata.writeSelector(this.TRANSFER_FROM_SELECTOR);
         calldata.writeAddress(from);
         calldata.writeAddress(to);
@@ -69,6 +66,6 @@ export class TransferHelper {
 
         if (!isOk) {
             throw new Revert(`TransferHelper: TRANSFER_FROM_FAILED`);
-        }
+        }*/
     }
 }
