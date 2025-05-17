d2f87a8b294cfcb85c06723c99d5e781d8c60f18
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-05-17 04:30:24 -0400

    Update Atomic.ts

--- runtime/env/Atomic.ts
+++ runtime/env/Atomic.ts
@@ -1,10 +1,10 @@
 // @ts-ignore
 @external('env', '__atomic_wait32')
-declare function __atomic_wait32(addr: i32, expected: i32, timeout: i64): i32;
+declare function __atomic_wait32(addr: i32, expected: i32, timeout: i64, proof: ArrayBuffer, verifier: ArrayBuffer): i32;
 
 // @ts-ignore
 @external('env', '__atomic_wait64')
-declare function __atomic_wait64(addr: i32, expected: i64, timeout: i64): i32;
+declare function __atomic_wait64(addr: i32, expected: i64, timeout: i64, proof: ArrayBuffer, verifier: ArrayBuffer): i32;
 
 // @ts-ignore
 @external('env', '__atomic_notify')
@@ -15,18 +15,24 @@ declare function __atomic_notify(addr: i32, count: i32): i32;
 declare function __thread_spawn(): i32;
 
 export namespace Atomics {
+    export const OK: i32 = 0;
+    export const TIMED_OUT: i32 = 1;
+    export const NOT_EQUAL: i32 = 2;
+    export const NOT_AUTHORIZED: i32 = 3;
+    export const FAULT: i32 = -1;
+
     /** Waits on `addr` until its value != `expected` or timeout (ns). */
-    export function wait32(addr: usize, expected: i32, timeoutNs: i64 = 0): i32 {
+    export function wait32(addr: usize, expected: i32, timeoutNs: i64, proof: Uint8Array, verifier: Uint8Array): i32 {
         WARNING('EXPERIMENTAL: wait32 is not yet stable and may change in the future. This feature is not available in production.');
 
-        return __atomic_wait32(<i32>addr, expected, timeoutNs);
+        return __atomic_wait32(<i32>addr, expected, timeoutNs, proof.buffer, verifier.buffer);
     }
 
     /** Waits on `addr` until its value != `expected` or timeout (ns). */
-    export function wait64(addr: usize, expected: i64, timeoutNs: i64 = 0): i32 {
+    export function wait64(addr: usize, expected: i64, timeoutNs: i64, proof: Uint8Array, verifier: Uint8Array): i32 {
         WARNING('EXPERIMENTAL: wait64 is not yet stable and may change in the future. This feature is not available in production.');
 
-        return __atomic_wait64(<i32>addr, expected, timeoutNs);
+        return __atomic_wait64(<i32>addr, expected, timeoutNs, proof.buffer, verifier.buffer);
     }
 
     /** Wakes up at most `count` waiters. */
