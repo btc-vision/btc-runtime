c509653980d836bde300a9b93a03ad736772a920
matbout <48333440+matbout@users.noreply.github.com>
2025-06-19 02:23:17 +0400

    Update IOP20

--- runtime/contracts/interfaces/IOP20.ts
+++ runtime/contracts/interfaces/IOP20.ts
@@ -1,21 +1,15 @@
 import { BytesWriter } from '../../buffer/BytesWriter';
-import { StoredU256 } from '../../storage/StoredU256';
 import { Calldata } from '../../types';
 
 export interface IOP20 {
-    readonly _totalSupply: StoredU256;
-
+    domainSeparator(callData: Calldata): BytesWriter;
     balanceOf(callData: Calldata): BytesWriter;
-
-    safeTransfer(callData: Calldata): BytesWriter;
-
-    safeTransferFrom(callData: Calldata): BytesWriter;
-
-    increaseAllowance(callData: Calldata): BytesWriter;
-
-    decreaseAllowance(callData: Calldata): BytesWriter;
-
+    nonceOf(callData: Calldata): BytesWriter;
     allowance(callData: Calldata): BytesWriter;
-
-    burn(callData: Calldata): BytesWriter;
+    safeTransfer(callData: Calldata): BytesWriter;
+    safeTransferFrom(callData: Calldata): BytesWriter;
+    increaseAllowance(callData: Calldata): BytesWriter;
+    decreaseAllowance(callData: Calldata): BytesWriter;
+    increaseAllowanceBySignature(callData: Calldata): BytesWriter;
+    decreaseAllowanceBySignature(callData: Calldata): BytesWriter;
 }
