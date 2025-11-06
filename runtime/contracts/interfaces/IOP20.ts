0c93511894bdc1ace722260ed5e3c154599edeeb
matbout <48333440+matbout@users.noreply.github.com>
2025-11-06 14:02:35 -0500

    Add missing functions to IOP20 interface and create IOP20Receiver interface

--- runtime/contracts/interfaces/IOP20.ts
+++ runtime/contracts/interfaces/IOP20.ts
@@ -2,12 +2,21 @@ import { BytesWriter } from '../../buffer/BytesWriter';
 import { Calldata } from '../../types';
 
 export interface IOP20 {
+    name(callData: Calldata): BytesWriter;
+    symbol(callData: Calldata): BytesWriter;
+    icon(callData: Calldata): BytesWriter;
+    decimals(callData: Calldata): BytesWriter;
+    totalSupply(callData: Calldata): BytesWriter;
     domainSeparator(callData: Calldata): BytesWriter;
+    metadata(callData: Calldata): BytesWriter;
     balanceOf(callData: Calldata): BytesWriter;
     nonceOf(callData: Calldata): BytesWriter;
     allowance(callData: Calldata): BytesWriter;
     safeTransfer(callData: Calldata): BytesWriter;
     safeTransferFrom(callData: Calldata): BytesWriter;
+    transfer(callData: Calldata): BytesWriter;
+    transferFrom(callData: Calldata): BytesWriter;
+    burn(callData: Calldata): BytesWriter;
     increaseAllowance(callData: Calldata): BytesWriter;
     decreaseAllowance(callData: Calldata): BytesWriter;
     increaseAllowanceBySignature(callData: Calldata): BytesWriter;
