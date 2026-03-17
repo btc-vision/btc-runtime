811610bf9993e465385e88dab580794c0b53a25f
matbout <48333440+matbout@users.noreply.github.com>
2026-03-16 21:32:24 -0400

    Rename field in OP721ApprovedEvent

--- runtime/events/predefined/OP721ApprovedEvent.ts
+++ runtime/events/predefined/OP721ApprovedEvent.ts
@@ -6,10 +6,10 @@ import { NetEvent } from '../NetEvent';
 
 @final
 export class OP721ApprovedEvent extends NetEvent {
-    constructor(owner: Address, spender: Address, tokenId: u256) {
+    constructor(owner: Address, operator: Address, tokenId: u256) {
         const data: BytesWriter = new BytesWriter(ADDRESS_BYTE_LENGTH * 2 + U256_BYTE_LENGTH);
         data.writeAddress(owner);
-        data.writeAddress(spender);
+        data.writeAddress(operator);
         data.writeU256(tokenId);
 
         super('Approved', data);
